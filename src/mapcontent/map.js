9496c186b0800a532cc1f14094af8fe8c901129f
luancr2173 <luan.ramos.al@gmail.com>
2025-09-02 14:57:32 +0000

    feat: drawingManager implementado

--- src/mapcontent/map.js
+++ src/mapcontent/map.js
@@ -1,38 +1,45 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import DrawingManager from "./DrawingManager";
 
 const Map = () => {
   const mapRef = useRef(null);
+  const [map, setMap] = useState(null);
 
   useEffect(() => {
     const initMap = () => {
-      new window.google.maps.Map(mapRef.current, {
+      const mapInstance = new window.google.maps.Map(mapRef.current, {
         center: { lat: -15.793889, lng: -47.882778 }, // Brasília
         zoom: 12,
       });
+      setMap(mapInstance); // salva no estado
     };
 
-    // Só injeta se ainda não tiver script
     if (!document.getElementById("google-maps-script")) {
       const script = document.createElement("script");
       script.id = "google-maps-script";
-      script.src = `https://maps.googleapis.com/maps/api/js?key=`; 
+      script.src = `https://maps.googleapis.com/maps/api/js?key=&libraries=drawing`;
       script.async = true;
       script.defer = true;
 
-      script.onload = () => {
-        initMap(); // garante que só roda quando o Google Maps já carregou
-      };
-
+      script.onload = () => initMap();
       document.body.appendChild(script);
     } else {
-      // Se já tá carregado, inicializa direto
       if (window.google && window.google.maps) {
         initMap();
       }
     }
   }, []);
 
-  return <div ref={mapRef} className="map-container" />;
+  return (
+    <>
+      <div
+        ref={mapRef}
+        className="map-container"
+        style={{ height: "100vh", width: "100%" }}
+      />
+      {map && <DrawingManager map={map} />}
+    </>
+  );
 };
 
 export default Map;
